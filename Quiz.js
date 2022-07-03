8eaa9c68db64195647181c9de79b90cc1000e4ff
MirzaAyon <ayonmirza98@gmail.com>
2022-07-03 16:29:34 +0600

    some interview ques added

---
+++ Quiz.js
@@ -0,0 +1,25 @@
+// var mahiya = {mahi: “hate”};
+// How will you change or update the mahi property of the mahiya object?
+
+// ans : mahiya.mahi = 'love';
+
+// 5
+// What will the break do in a loop?
+// ans : break the loop and end the loop immediately
+
+// 8
+// What will the continue keyword do in a loop?
+// skip the rest code of the current iteration and goes to the next
+
+// 10
+// Is there anything wrong with this code?
+
+//  functon add(num1, num2){ }
+//function banan bhul
+
+12
+How you can get the brand name from object smartphone ? 
+
+var smartphone = { brand: "iPhone", model: "13" };
+// console.log(smartphone.brand);
+// console.log(smartphone['brand']);
